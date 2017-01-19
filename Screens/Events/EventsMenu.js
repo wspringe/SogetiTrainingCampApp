3f94ce19b352667bcc7984355c15ceaa13f769cf
Wesley Springer <wesleyspringer@Wesleys-MacBook-Pro.local>
2017-01-19 13:58:01 -0700

    Conditional rendering based on role done

--- Screens/Events/EventsMenu.js
+++ Screens/Events/EventsMenu.js
@@ -9,7 +9,7 @@ import {
   ScrollView 
 } from 'react-native';
 import Drawer from 'react-native-drawer';
-import ControlPanel from '../Control Panels/ControlPanelAdmin';
+import ControlPanel from '../Control Panels/ControlPanel';
 import NavigationBar from 'react-native-navbar';
 import MenuIcon from '../../Icons JS/MenuIcon';
 
@@ -51,7 +51,7 @@ export default class MainMenu extends Component {
         type="overlay"
         //This is where menu goes on sidebar with props passing to ControlPanel
         content={
-          <ControlPanel closeDrawer={this.closeDrawer} name={this.props.profile.name} avatar={this.props.profile.picture}/>
+          <ControlPanel closeDrawer={this.closeDrawer} name={this.props.profile.name} avatar={this.props.profile.picture} role={this.props.profile.roles}/>
         }
         tapToClose={true}
         styles={{main: {shadowColor: '#000', shadowOpacity: 0.5, shadowRadius: 15}}}
@@ -131,6 +131,7 @@ export default class MainMenu extends Component {
           </View>
           <View style={styles.hintTextContainer}>
             <Text style={styles.hintText}>Press the 3 horizontal black lines to open the menu!</Text>
+            <Text style={styles.hintText}>{this.props.profile.roles}</Text>
           </View>
         </ScrollView>
         
