053d519359e782aca45f39b488a2829416c3950b
Wesley Springer <wesleyspringer@Wesleys-MacBook-Pro.local>
2017-01-19 13:57:02 -0700

    Test JSON

--- Screens/SplashScreen.js
+++ Screens/SplashScreen.js
@@ -15,6 +15,36 @@ var credentials = require('../Auth-0/auth0-credentials');
 var lock = new Auth0Lock(credentials);
 
 export default class SplashScreen extends Component{
+
+  //need initial prop(s) so that the json data can be assigned to a state and used
+  constructor(props) {
+    super(props);
+    this.state = {
+      jsonData: ''
+    }
+  }
+
+//needed to call json function to get data
+  componentDidMount() {
+    this.loadJsonData();
+  }
+
+//json function to get json data; almost everything is copied 
+//from https://facebook.github.io/react-native/docs/network.html
+  loadJsonData() {
+    fetch('http://sample-env.uagzrjthc3.us-west-2.elasticbeanstalk.com/name')
+      .then((response) => response.json()) 
+      .then((responseJson) => { 
+        //sets state of previously declared state
+        this.setState({
+          jsonData: responseJson
+        })
+      }) 
+        .catch((error) => { 
+          console.error(error); 
+        });
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -25,6 +55,9 @@ export default class SplashScreen extends Component{
           />
           <Text style={styles.title}>Sogeti Training Camp</Text>
           <Text style={styles.subtitle}>by ASU Capstone</Text>
+          <Text style={styles.subtitle}>Name: {this.state.jsonData.name}</Text>
+          <Text style={styles.subtitle}>Status: {this.state.jsonData.status}</Text>
+          
         </View>
         <TouchableHighlight
           style={styles.signInButton}
