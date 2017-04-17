967f41fd17e057e3f52cbaee06f1c493b6ebf1dc
Eric Trang <edtrang@asu.edu>
2017-04-16 18:48:21 -0700

    fixed events route

--- Screens/Events/AddEvent.js
+++ Screens/Events/AddEvent.js
@@ -21,37 +21,41 @@ import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scrollview'
 import NavigationBar from 'react-native-navbar';
 import BackButtonIcon from '../../Icons JS/BackButtonIcon';
 
-var newEvent;
+var newEvent = {};
 
 export default class AddEvent extends Component {
   //var newEvent;
   
-  /*
+  
   createEvent() {
-    method: 'POST',
-    body: JSON.stringify({
-      name: 'yourValue',
-      description: 'yourOtherValue',
-      date: '',
-      starttime,
-      endtime
+    fetch('http://forums.swirlclinic.com:1337/api/events', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: newEvent
     })
   }
-  */
+  
 
   render() {
     const rightButtonConfig = {
       title: 'Save',
       color: 'white',
-      handler: () => { alert('Saved')
-                       this.props.navigator.pop() }
+      handler: () => { 
+                        alert('Saved');
+                        this.createEvent();
+                        console.log(newEvent);
+                       this.props.navigator.pop()
+                      }
     }
     const titleConfig = {
       title: 'Add Event',
       color: 'black'
     }
 
-    const CellEdit = (props) => (
+    const CellName = (props) => (
       <Cell
         {...props}
         cellContentView={
@@ -66,6 +70,105 @@ export default class AddEvent extends Component {
               {props.title}
             </Text>
             <TextInput
+              onChangeText={(text) => newEvent.name = text}
+              style={{width: 200}}
+              placeholder={props.placeholder}>
+            </TextInput>
+          </View>
+          }
+        />
+      );
+
+      const CellStart = (props) => (
+      <Cell
+        {...props}
+        cellContentView={
+          <View
+            style={{ alignItems: 'center', flexDirection: 'row', flex: 1, paddingVertical: 5 }}
+              >
+            <Text
+              allowFontScaling
+              numberOfLines={1}
+              style={{ flex: 1, fontSize: 20 }}
+                >
+              {props.title}
+            </Text>
+            <TextInput
+              onChangeText={(text) => newEvent.starttime = text}
+              style={{width: 200}}
+              placeholder={props.placeholder}>
+            </TextInput>
+          </View>
+          }
+        />
+      );
+
+      const CellEnd = (props) => (
+      <Cell
+        {...props}
+        cellContentView={
+          <View
+            style={{ alignItems: 'center', flexDirection: 'row', flex: 1, paddingVertical: 5 }}
+              >
+            <Text
+              allowFontScaling
+              numberOfLines={1}
+              style={{ flex: 1, fontSize: 20 }}
+                >
+              {props.title}
+            </Text>
+            <TextInput
+              onChangeText={(text) => newEvent.endtime = text}
+              style={{width: 200}}
+              placeholder={props.placeholder}>
+            </TextInput>
+          </View>
+          }
+        />
+      );
+
+      const CellDate = (props) => (
+      <Cell
+        {...props}
+        cellContentView={
+          <View
+            style={{ alignItems: 'center', flexDirection: 'row', flex: 1, paddingVertical: 5 }}
+              >
+            <Text
+              allowFontScaling
+              numberOfLines={1}
+              style={{ flex: 1, fontSize: 20 }}
+                >
+              {props.title}
+            </Text>
+            <TextInput
+              onChangeText={(text) => newEvent.date = text}
+              style={{width: 200}}
+              placeholder={props.placeholder}>
+            </TextInput>
+          </View>
+          }
+        />
+      );
+
+      
+
+      const CellHost = (props) => (
+      <Cell
+        {...props}
+        cellContentView={
+          <View
+            style={{ alignItems: 'center', flexDirection: 'row', flex: 1, paddingVertical: 5 }}
+              >
+            <Text
+              allowFontScaling
+              numberOfLines={1}
+              style={{ flex: 1, fontSize: 20 }}
+                >
+              {props.title}
+            </Text>
+            <TextInput
+              onChangeText={(text) => newEvent.host = text}
               style={{width: 200}}
               placeholder={props.placeholder}>
             </TextInput>
@@ -91,6 +194,7 @@ export default class AddEvent extends Component {
             <TextInput
               style={{width: 200, textAlignVertical: 'top', height: 100}}
               multiline={true}
+              onChangeText={(text) => newEvent.description = text}
               placeholder={props.placeholder}>
             </TextInput>
           </View>
@@ -111,17 +215,22 @@ export default class AddEvent extends Component {
             tintColor='orange'
           /> 
           <KeyboardAwareScrollView>
-            <TableView onPress={(event) => console.log(event)}>
+            <TableView>
               <Section>
-                <CellEdit title="Event Name: " placeholder="Event Name" />
-                <CellEdit title="Current Time: " placeholder="Current Time" />
-                <CellEdit title="End Time: " placeholder="End Time" />
-                <CellEdit title="Host: " placeholder="Host Name" />
+                <CellName title="Event Name: " placeholder="Event Name" />
+
+                <CellDate title="Date: " placeholder="Date" />
+                <CellStart title="Start Time: " placeholder="Start Time" />
+                <CellEnd title="End Time: " placeholder="End Time" />
+
+                <CellHost title="Host: " placeholder="Host Name" />
+                
                 <CellDescription title= "Description:" placeholder="Description..." />
               </Section>
             </TableView>
           </KeyboardAwareScrollView>
         </View>
+        
       )
   }
 }
